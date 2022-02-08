1ad07cc7b97617b259164049ed92acead5f6d5c0
lehakot-create <lehakot85@inbox.ru>
2022-02-08 11:52:23 +0400

    make it better

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -126,11 +126,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
