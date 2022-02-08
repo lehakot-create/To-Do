a5aeee1ae2e03f817b2fbb42b3c7a7d828d83a56
lehakot-create <lehakot85@inbox.ru>
2022-02-08 11:56:44 +0400

    make it better

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -125,10 +125,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
