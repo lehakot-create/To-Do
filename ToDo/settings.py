f93590cad872c857eb38a47d0829d8e1ba4a3646
lehakot-create <lehakot85@inbox.ru>
2022-02-08 11:59:31 +0400

    Diploy

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,5 @@ LOGIN_REDIRECT_URL = '/'
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
